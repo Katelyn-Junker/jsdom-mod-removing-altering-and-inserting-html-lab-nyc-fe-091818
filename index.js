e3ff53a6f5eb98ffad366d0233dcbbeeb8947fce
Katelyn Junker <kjunker@villanova.edu>
2018-10-17 00:22:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /* Enter the code to remove the main node element under this comment */
+document.queryElement('main').remove()
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+let newHeader= document.createElement('h1'); 
+newHeader.innerText= Katelyn;
+body.appendeChild(newHeader);
